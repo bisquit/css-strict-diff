bbf91239dae4ba5bb2626c75e8ba7f847e04022c
shuheikikuchi <qu1dpr09u0@gmail.com>
2023-06-05 17:25:57 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,53 @@
+import microdiff from 'microdiff';
 import { parse } from 'postcss';
 
-export function scopeString(css: string) {
-  return `
-    #root {
-      ${css}
-    }
-  `;
+/**
+ * PostCSS API
+ * https://postcss.org/api/
+ */
+
+type Selector = string;
+
+type Decl = {
+  prop: string;
+  value: string;
+};
+
+type RuleMap = Map<Selector, Decl[]>;
+type RuleObj = { [key: Selector]: Decl[] };
+
+export function diff(baseCss: string, afterCss: string) {
+  const baseCssRuleMap = toRuleMap(baseCss);
+  const afterCssRuleMap = toRuleMap(afterCss);
+
+  const result = microdiff(baseCssRuleMap, afterCssRuleMap);
+  console.log('result', result);
+}
+
+export function toRuleMap(css: string): RuleObj {
+  const root = parse(css);
+
+  const map = new Map() as RuleMap;
+  const result = {} as RuleObj;
+
+  root.walkRules((rule) => {
+    const decls = [] as { prop: string; value: string }[];
+    rule.walkDecls((decl) => {
+      const { prop, value } = decl;
+      decls.push({ prop, value });
+    });
+
+    rule.selectors.forEach((selector) => {
+      // map.set(selector, [...(map.get(selector) ?? []), ...decls]);
+      result[selector] = [...(result[selector] ?? []), ...decls];
+    });
+  });
+
+  console.log(result);
+
+  return result;
+}
+
+function expandShorthand(prop: string, value: string) {
+  return undefined;
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,16 +1,39 @@
 import { expect, test } from 'vitest';
 
-import { scopeString } from '../src';
+import { diff, toRuleMap } from '../src';
 
-test('scopeString', async () => {
-  const result = scopeString(`
-    h1 {
+test('diff', async () => {
+  const result = diff(
+    `
+    h1, h2 {
       margin: 0;
+      padding: 0;
     }
     p {
       color: red;
     }
-  `);
+    h1 {
+      font-size: 1rem;
+    }
+    div h1 {
+      font-size: 2rem;
+    }
+  `,
+    `
+    h1, h2 {
+      margin: 0;
+      padding: 0;
+    }
+    p {
+      color: red;
+      background: blue;
+    }
+    div h1 {
+      font-size: 3rem;
+      font-weight: bold;
+    }
+    `
+  );
 
   expect(result).toBe(`
     #root {
